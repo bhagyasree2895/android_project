9e9accc718144d91af2a36152d67332d97ef46e0
nikitha <nikithalakmarapu@gmail.com>
2019-10-04 21:43:23 -0500

    committed after creating postavailability,uploadimage,submit activities.

--- GetRoomieCode/app/src/main/java/com/example/getroomiecode/PostingAvailabilityActivity.java
+++ GetRoomieCode/app/src/main/java/com/example/getroomiecode/PostingAvailabilityActivity.java
@@ -2,7 +2,9 @@ package com.example.getroomiecode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PostingAvailabilityActivity extends AppCompatActivity {
 
@@ -10,5 +12,34 @@ public class PostingAvailabilityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posting_availability);
+    }
+    public void gotohomePage(View v) {
+        try {
+            Intent toOtherIntent = new Intent(this,SignInActivity.class);
+            startActivity(toOtherIntent);
+
+        } catch (Exception e) {
+
+        }
+    }
+    public void gotouploadimage(View v) {
+        try {
+            Intent toOtherIntent = new Intent(this,uploadImageActivity.class);
+            startActivity(toOtherIntent);
+
+        } catch (Exception e) {
+
+        }
+
+    }
+    public void onSubmit(View v) {
+        try {
+            Intent toOtherIntent = new Intent(this,submitActivity.class);
+            startActivity(toOtherIntent);
+
+        } catch (Exception e) {
+
+        }
+
     }
 }
---
+++ GetRoomieCode/app/src/main/java/com/example/getroomiecode/submitActivity.java
@@ -0,0 +1,25 @@
+package com.example.getroomiecode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class submitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit);
+    }
+    public void gotohomePage(View v) {
+        try {
+            Intent toOtherIntent = new Intent(this,SignInActivity.class);
+            startActivity(toOtherIntent);
+
+        } catch (Exception e) {
+
+        }
+    }
+}
