507f09965d631b293e4be5e89ac4aa9e59ce0f54
nikitha <nikithalakmarapu@gmail.com>
2019-10-25 19:42:15 -0500

    added findRoom button in SignInActivity.java and added its functionality

---
+++ GetRoomieCode/app/src/main/java/com/example/getroomiecode/RoomsListView.java
@@ -0,0 +1,61 @@
+package com.example.getroomiecode;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+
+public class RoomsListView extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rooms_list_view);
+        ArrayList<RoomItem> roomList = new ArrayList<>();
+        roomList.add(new RoomItem(R.drawable.roomingone, "Horizons", "250$"));
+        roomList.add(new RoomItem(R.drawable.roomingtwo, "Village O", "190$"));
+        roomList.add(new RoomItem(R.drawable.roomingone, "FoxAlley", "210$"));
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        adapter = new ItemAdapter(roomList);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.room_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //  adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
+    }
+}
--- GetRoomieCode/app/src/main/java/com/example/getroomiecode/SignInActivity.java
+++ GetRoomieCode/app/src/main/java/com/example/getroomiecode/SignInActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.SearchView;
 
@@ -23,69 +25,48 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        ArrayList<RoomItem> roomList = new ArrayList<>();
-        roomList.add(new RoomItem(R.drawable.roomingone, "Horizons", "250$"));
-        roomList.add(new RoomItem(R.drawable.roomingtwo, "Village O", "190$"));
-        roomList.add(new RoomItem(R.drawable.roomingone, "FoxAlley", "210$"));
-        mRecyclerView = findViewById(R.id.recyclerView);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        adapter = new ItemAdapter(roomList);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(adapter);
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.room_menu, menu);
+    //Search room availability
+    public void gotoSearchRoom(View v) {
+        try {
+            Intent toOtherIntent = new Intent(this,RoomsListView.class);
+            startActivity(toOtherIntent);
 
-        MenuItem searchItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) searchItem.getActionView();
+        } catch (Exception e) {
 
-        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        }
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-              //  adapter.getFilter().filter(newText);
-                return false;
-            }
-        });
-        return true;
     }
-//    //Post Availability action
-//    public void gotoPostingAvailAction(View v) {
-//        try {
-//            Intent toOtherIntent = new Intent(this, PostingAvailabilityActivity.class);
-//            startActivity(toOtherIntent);
-//
-//        } catch (Exception e) {
-//
-//        }
-//
-//
-//    }
-//    public void gotosignoutAction(View v) {
-//        try {
-//            Intent toOtherIntent = new Intent(this, MainActivity.class);
-//            startActivity(toOtherIntent);
-//
-//        } catch (Exception e) {
-//
-//        }
-//    }
-//    public void onSearch(View v) {
-//        try {
-//            Intent toOtherIntent = new Intent(this, SearchResultActivity.class);
-//            startActivity(toOtherIntent);
-//
-//        } catch (Exception e) {
-//
-//        }
-//    }
+
+    //Post Availability action
+    public void gotoPostingAvailAction(View v) {
+        try {
+            Intent toOtherIntent = new Intent(this, PostingAvailabilityActivity.class);
+            startActivity(toOtherIntent);
+
+        } catch (Exception e) {
+
+        }
+
+
+    }
+    public void gotosignoutAction(View v) {
+        try {
+            Intent toOtherIntent = new Intent(this, MainActivity.class);
+            startActivity(toOtherIntent);
+
+        } catch (Exception e) {
+
+        }
+    }
+    public void onSearch(View v) {
+        try {
+            Intent toOtherIntent = new Intent(this, SearchResultActivity.class);
+            startActivity(toOtherIntent);
+
+        } catch (Exception e) {
+
+        }
+    }
 }
