e4a9acff65206c519325d6813d8e29d7f26bbc33
nikitha <nikithalakmarapu@gmail.com>
2019-10-25 20:45:27 -0500

    Error rectification:changed textview of apt type to pincode in activity_posting_availability.xml

--- GetRoomieCode/app/src/main/java/com/example/getroomiecode/SignUpActivity.java
+++ GetRoomieCode/app/src/main/java/com/example/getroomiecode/SignUpActivity.java
@@ -83,20 +83,16 @@ public class SignUpActivity extends AppCompatActivity {
             try {
                 Intent toOtherIntent = new Intent(this, SuccessfulSignUpActivity.class);
                 startActivity(toOtherIntent);
-
             } catch (Exception e) {
-
             }
         }
     }
     public static boolean isValidPassword(final String password) {
-
         Pattern pattern;
         Matcher matcher;
         final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
         pattern = Pattern.compile(PASSWORD_PATTERN);
         matcher = pattern.matcher(password);
-
         return matcher.matches();
 
     }
